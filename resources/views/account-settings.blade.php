714be103dae6c94998ae85fc0048f8fa72244edd
JakeobC <jacun24@student.sdu.dk>
2025-11-06 12:30:13 +0100

    Add basic Account Settings page  with controller, routes, views, and styles

---
+++ resources/views/account-settings.blade.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <title>RecyShare</title>
+</head>
+
+<body>
+    <header>
+        <nav class="navbar">
+            <div class="left">
+                <div class="logo">
+                    <img src="{{ asset('assets/logos/recyshare-logo-no-text.png') }}" alt="RecyShare Logo">
+                    <h3>RecyShare</h3>
+                </div>
+            </div>
+            <div class="right">
+                <ul class="nav-links">
+                    <li><a href="{{ route('home') }}">Home</a></li>
+                    <li><a href="{{ route('about') }}">About</a></li>
+                    <li><a href="{{ route('recipes.index') }}">Recipes</a></li>
+                    <li class="dropdown">
+                        <span class="dropbtn">Categories</span>
+                        <div class="dropdown-content">
+                            <a href="#">Breakfast</a>
+                            <a href="#">Lunch</a>
+                            <a href="#">Dinner</a>
+                            <a href="#">Dessert</a>
+                            <a href="#">Vegan</a>
+                            <a href="#">Gluten-Free</a>
+                        </div>
+                    </li>
+                </ul>
+                <div class="search-bar">
+                    <img class="icon" src="{{ asset('assets/icons/search_48dp_000000_FILL0_wght300_GRAD200_opsz48.png') }}"
+                        alt="Search">
+                    <input type="search" id="searchBar" placeholder="Search...">
+                </div>
+                <div class="account">
+                    <img class="icon" src="{{ asset('assets/icons/account_circle_48dp_000000_FILL0_wght300_GRAD200_opsz48.png') }}"
+                        alt="Account">
+                </div>
+            </div>
+        </nav>
+    </header>
+    <main>
+     <div class="settings-form">
+        
+
+    </div>   
+    </main>
+
+    
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\RecipeController;
+use App\Http\Controllers\AccountSettingsController;
 use Illuminate\Support\Facades\Route;
 
 Route::redirect('/', '/home');
@@ -17,3 +18,7 @@ Route::get('/share-a-recipe', [RecipeController::class, 'create'])->name('recipe
 
 // About
 Route::get('/about', [AboutController::class, 'index'])->name('about');
+
+// Account Settings
+Route::get('/account-settings', [AccountSettingsController::class, 'index'])->name('account-settings.index');
+Route::post('/account-settings', [AccountSettingsController::class, 'update'])->name('account-settings.update');
