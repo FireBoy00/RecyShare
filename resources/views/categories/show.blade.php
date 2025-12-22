c07b51031e92b31651a2a964a5a97fea23aa6e74
TZarins <tomasszarinsh@gmail.com>
2025-12-22 19:47:55 +0200

    Fixed the recipe deletion issue for categories

--- resources/views/categories/show.blade.php
+++ resources/views/categories/show.blade.php
@@ -3,7 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/css/recipes.css'])
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/css/recipe-card.css', 'resources/js/recipe-card.js'])
+    @vite(['resources/css/recipes.css', 'resources/js/recipes.js'])
     <title>{{ $category }} Recipes</title>
 </head>
 
